709d9a78d35e3a20818b8800f4c097f84c5dccc6
Raj Bharath AgileLab <raj.bharath@agilelab.it>
2022-08-18 10:55:15 +0530

    Name formation

--- arguments-and-side-effects/argumentsexample/ArgumentsExample.java
+++ arguments-and-side-effects/argumentsexample/ArgumentsExample.java
@@ -5,19 +5,25 @@ import java.util.Map;
 
 public class ArgumentsExample {
   public static void main(String[] args) {
-    Map<String, String> enhancedUserDetails =  getEnhancedUserDetails("Karthee", "Nataraj", Gender.MALE, "123123");  
+    Map<String, String> enhancedUserDetails = getEnhancedUserDetails("Karthee", "Nataraj", Gender.MALE, "123123");
     System.out.println(enhancedUserDetails);
   }
 
-
-  public static Map<String, String> getEnhancedUserDetails(String firstName, String lastName, Gender gender, String pincode) {
+  public static Map<String, String> getEnhancedUserDetails(String firstName, String lastName, Gender gender,
+      String pincode) {
     String greeting = gender.greeting();
-    String name = String.format("%s %s %s", greeting, firstName, lastName);
+    String name = formatName(greeting, firstName, lastName);
     String address = findAddressFor(pincode);
-    return new HashMap<String, String>() {{
-      put("name", name);
-      put("address", address);
-    }};
+    return new HashMap<String, String>() {
+      {
+        put("name", name);
+        put("address", address);
+      }
+    };
+  }
+
+  public static String formatName(String greeting, String firstName, String lastName) {
+    return String.format("%s %s %s", greeting, firstName, lastName);
   }
 
   public static String findAddressFor(String pincode) {
@@ -28,6 +34,7 @@ public class ArgumentsExample {
 interface Gender {
   public static final Gender MALE = new MALE();
   public static final Gender FEMALE = new FEMALE();
+
   public String greeting();
 }
 
@@ -36,6 +43,7 @@ class MALE implements Gender {
     return "Mr.";
   }
 }
+
 class FEMALE implements Gender {
   public String greeting() {
     return "Mrs.";
