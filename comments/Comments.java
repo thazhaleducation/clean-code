a8dc42acbf2be927876f54014830929e3db0a764
Raj Bharath AgileLab <raj.bharath@agilelab.it>
2022-08-20 10:08:20 +0530

    Avoid commented code in the code base. It will mislead the reader

--- comments/Comments.java
+++ comments/Comments.java
@@ -1,9 +1,18 @@
 public class Comments {
   public static void main(String[] args) {
-    int i = 1; // Initialize the counter to 1
-    while (i <= 10) { // While counter is less than or equal to 10
-      System.out.println("No of tickets sold:" + i);
-      i = i + 1; // increment the counter by 1
+    int numberOfSoldTickets = 1;
+    while (hasTickets(numberOfSoldTickets)) { 
+      System.out.println("No of tickets sold:" + numberOfSoldTickets);
+      numberOfSoldTickets = numberOfSoldTickets + 1;
     }
   }
+
+  public static boolean hasTickets(int counter) {
+    int MAX_NO_OF_TICKETS  = 10;
+    return counter <= MAX_NO_OF_TICKETS;
+  }
+
+  // public static void increaseTickets() {
+  //   // Do some logic to increase tickets
+  // }
 }
