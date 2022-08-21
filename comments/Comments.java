2853f6dae6bc0974c3ef702386adc2f40715bc87
Raj Bharath AgileLab <raj.bharath@agilelab.it>
2022-08-21 21:10:59 +0530

    Avoid marker comments

--- comments/Comments.java
+++ comments/Comments.java
@@ -1,13 +1,15 @@
 public class Comments {
+
   public static void main(String[] args) {
     int numberOfSoldTickets = 1;
     while (hasTickets(numberOfSoldTickets)) { 
       System.out.println("No of tickets sold:" + numberOfSoldTickets);
       // My team lead asked not to use increment operator. So used '+'
       numberOfSoldTickets = numberOfSoldTickets + 1;
-    }
-  }
+    } //while
+  } // main function
 
+  // ######### Has tickets ########
   public static boolean hasTickets(int counter) {
     // Very small hall so only it has 10 tickets. I would not go here.
     int MAX_NO_OF_TICKETS  = 10;
