eb5cfca5e4a3e49449c798f513644deb77e670c9
Raj Bharath AgileLab <raj.bharath@agilelab.it>
2022-09-10 23:18:07 +0530

    Data abstractions - added change requests

--- data-abstraction/Procedural.java
+++ data-abstraction/Procedural.java
@@ -14,7 +14,13 @@ public class Procedural {
 
     // Pay via card
     Invoice invoice2 = new Invoice();
-    invoice2.pay(new Card("1234123412341234",123));
+    invoice2.pay(new Card("1234123412341234", 123));
+
+    // Pay and Refund via upi
+    Invoice invoice3 = new Invoice();
+    invoice3.pay(new Upi("123123@upi"));
+    invoice3.refund(new Upi("123123@upi"));
+    
   }
 }
 
@@ -27,14 +33,36 @@ class Invoice {
       Card card = (Card) paymentMethod;
       sendRequestToVISA(card.cardNumber, card.pin);
       System.out.println("Amount paid in card");
+    } else if (paymentMethod instanceof Upi) {
+      Upi upi = (Upi) paymentMethod;
+      sendRequestUpiGateway(upi.upiId);
+      System.out.println("Paid via Upi " + upi.upiId);
     } else {
       throw new UnsupportedPaymentMethodException();
     }
-    
+
+  }
+
+
+  public void refund(Object paymentMethod) throws UnsupportedPaymentMethodException {
+    if (paymentMethod instanceof Cash) {
+      Cash cash = (Cash) paymentMethod;
+      System.out.println("Amount refunded" + cash.amount);
+    } else if (paymentMethod instanceof Upi) {
+      Upi upi = (Upi) paymentMethod;
+      sendRequestUpiGateway(upi.upiId);
+      System.out.println("Refunded via Upi " + upi.upiId);
+    } else {
+      throw new UnsupportedPaymentMethodException();
+    }
   }
 
   private void sendRequestToVISA(String cardNumber, int pin) {
     System.out.println("Sending Request to VISA with card number and pin" + cardNumber + " " + pin);
+  }
+
+  private void sendRequestUpiGateway(String upiId) {
+    System.out.println("Sending request to Upi gateway");
   }
 
 }
@@ -53,8 +81,17 @@ class Cash {
 class Card {
   String cardNumber;
   int pin;
+
   public Card(String cardNumber, int pin) {
     this.cardNumber = cardNumber;
     this.pin = pin;
+  }
+}
+
+class Upi {
+  String upiId;
+
+  public Upi(String upiId) {
+    this.upiId = upiId;
   }
 }
