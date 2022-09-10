2b6d4e376c76e442af84f506fa414c24dc230b16
Raj Bharath AgileLab <raj.bharath@agilelab.it>
2022-09-10 18:23:42 +0530

    Add exception for data abstraction

--- data-abstraction/Procedural.java
+++ data-abstraction/Procedural.java
@@ -1,38 +1,44 @@
-public class Procedural{
-   public static void main(String[] args) {
+public class Procedural {
+  public static void main(String[] args) {
+    try {
+      payInvoice();
+    } catch (UnsupportedPaymentMethodException e) {
+      System.err.println(e.toString());
+    }
+  }
+
+  public static void payInvoice() {
     Invoice i = new Invoice();
     i.pay(new Cash(10));
-   }
+  }
 }
-
-
-
 
 class Invoice {
   public void pay(Object paymentMethod) throws UnsupportedPaymentMethodException {
     if (paymentMethod instanceof Cash) {
       Cash cash = (Cash) paymentMethod;
       System.out.println("Amount given in hand" + cash.amount);
-    } else if(paymentMethod instanceof Card) {
+    } else if (paymentMethod instanceof Card) {
       Card card = (Card) paymentMethod;
       sendRequestToVISA(card.cardNumber, card.pin);
       System.out.println("Amount paid in card");
     } else {
-      throw  new UnsupportedPaymentMethodException();
+      throw new UnsupportedPaymentMethodException();
     }
   }
-  
+
   private void sendRequestToVISA(String cardNumber, int pin) {
     System.out.println("Sending Request to VISA with card number and pin" + cardNumber + " " + pin);
   }
 
 }
 
-
-class UnsupportedPaymentMethodException extends Exception{}
+class UnsupportedPaymentMethodException extends Exception {
+}
 
 class Cash {
   int amount;
+
   public Cash(int amount) {
     this.amount = amount;
   }
@@ -42,7 +48,3 @@ class Card {
   String cardNumber;
   int pin;
 }
-
-// class Upi {
-//   String upiId;
-// }
